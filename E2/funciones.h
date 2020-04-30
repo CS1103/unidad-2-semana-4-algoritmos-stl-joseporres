f2ff82c40ce16098cf114e9b1e5bd35055a81014
joseporres <joseporres@users.noreply.github.com>
2020-04-29 23:32:27 -0500

    Ejercicios resueltos

--- E2/funciones.h
+++ E2/funciones.h
@@ -9,50 +9,64 @@
 #include <algorithm>
 using namespace std;
 
+
+template<typename T>
+void swap(T xp, T yp)
+{
+    T temp = xp;
+    xp = yp;
+    yp = temp;
+}
+
 template<typename T, typename cn>
-int swaps(cn tren, T size_){
+int swaps(cn tren, T size_) {
+    int i, j, swaap = 0, cont;
+    char temporal, temporal2, max;
     cn temp = tren;
-    bool f =true, r=true;
-    int i=0, j = 0, swaap =0;
-    char temporal;
-    sort(temp.begin(),temp.end());
-
-    for (int k = 0; k < size_-1 ; ++k) {
-        if(tren == temp) {
-            swaap += 1;
-            return swaap;
-        }
-        else{
-            for (int l = 0; l < size_-1 ; ++l) {
-                if()
-
+    sort(temp.begin(), temp.end());
+    if (temp[size_ - 1] != tren[size_ - 1]) {
+        for (int k = 0; k < size_ - 1; ++k) {
+            max = tren[0];
+            if (tren[k] >= max) {
+                max = tren[k];
+                cont = k;
             }
         }
-    }
+
+        swaap = size_- (cont+1);
+
+        if (tren[0] == temp[size_-1] && tren[size_-1] == temp[0]&& size_>2)
+            swaap+=2;
+
+        temporal2 = tren[size_ - 1];
+        tren[size_ - 1] = tren[cont];
+        tren[cont] = temporal2;
 
     }
-//
-//    while(f){
-//        j = i;
-//        if(tren == temp)
-//            f = false;
-//        while(r){
-//            if(tren[j] == temp[j])
-//                r =false;
-//            else{
-//                temporal = tren[j];
-//                tren[j] = tren[j+1];
-//                tren[j+1] = temporal;
-//                swaap +=1;
-//
-//            }
-//        }
-//        i+=1;
-//        r =true;
-//    }
-//    cout<<"Optimal train swapping takes "<< swaap<< " swaps.";
-//};
+    if(tren == temp)
+        cout << "Optimal train swapping takes "<<swaap<<" swaps.";
+    else{
+    for(i = 0; i<size_-1; i++) {
+        for(j = i+1; j<size_-1; j++)
+        {
+            if(tren[j] < tren[i]) {
+                temporal = tren[i];
+                tren[i] = tren[j];
+                tren[j] = temporal;
+                swaap+=1;
+            }
 
+        }
+    }
+        cout << "Optimal train swapping takes "<<swaap<<" swaps.";
+    }
+
+
+
+
+
+
+};
 
 
 #endif //E2_FUNCIONES_H
